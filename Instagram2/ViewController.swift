485b1f555a4b73e7e0145faa217ee44b4a742bc3
Yu-suke441 <it.creative.pgm.738@gmail.com>
2020-11-22 20:15:04 +0900

    Initial Commit

---
+++ Instagram2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram2
+//
+//  Created by Yusuke Murayama on 2020/11/22.
+//  Copyright © 2020 Yusuke.Murayama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
